dcb3920a71507e104a5a1e56b79ad66fe323c8c7
이진형 (Jinhyoung Lee) <jhcode0@naver.com>
2025-03-20 11:02:06 +0900

    patch: delete 요청

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,5 @@
 import api from './axios'
+//get
 export async function getData<T>(url: string, params?: object): Promise<T> {
   try {
     const response = await api.get<T>(url, { params })
@@ -18,9 +19,20 @@ export async function postData<T>(url: string, data?: object): Promise<T> {
   }
 }
 
+//put
 export async function putData<T>(url: string, data?: object): Promise<T> {
   try {
     const response = await api.put<T>(url, data)
+    return response.data
+  } catch (error) {
+    throw error
+  }
+}
+
+// DELETE 요청 (데이터 삭제)
+export async function deleteData<T>(url: string): Promise<T> {
+  try {
+    const response = await api.delete<T>(url)
     return response.data
   } catch (error) {
     throw error
